c8126b297fd7e38fade7e9b5f32348e73307227f
proin <proin@proinlab.com>
2016-12-27 18:05:37 +0900

    [0.5.1] node-sync module

---
+++ language-module/nodesync/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = (()=> {
+    let lang_module = {};
+
+    lang_module.create = (script)=> {
+        let result = 'async function __async__main__() {\n';
+        result += script;
+        result += '\n};';
+        result += '\n__async__main__();\n';
+        return result;
+    };
+
+    return lang_module;
+})();
